b075985f0701f09e54bfc95e3bb307f192419710
ag.thanhz <ag.thanhz@gmail.com>
2022-03-31 21:24:56 +0700

    init flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('dnThanh'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ));
+}
